ca7e9636ef6c1ca9bc6d03c2435303fc9226f19a
thanhan25052003 <thanhan01236339441@gmail.com>
2024-05-19 19:17:58 +0700

    annthe176161 update contract in model

--- src/main/java/org/example/insurance_card_be/model/Contract.java
+++ src/main/java/org/example/insurance_card_be/model/Contract.java
@@ -1,4 +1,19 @@
 package org.example.insurance_card_be.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Contract {
+    private int ContractID;
+    private int CustomerID;
+    private String ContractInfo;
+    private String status;
+    private Date StartDate;
+    private Date EndDate;
 }
