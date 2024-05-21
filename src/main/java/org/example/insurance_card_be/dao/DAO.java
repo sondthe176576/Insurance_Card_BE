465cc91905d0419a72c3046d43793dee45ee29a7
son12102003 <sondthe176576@fpt.edu.vn>
2024-05-21 08:01:25 +0700

    Son demo Login

--- src/main/java/org/example/insurance_card_be/dao/DAO.java
+++ src/main/java/org/example/insurance_card_be/dao/DAO.java
@@ -1,8 +1,7 @@
 package org.example.insurance_card_be.dao;
 
 public class DAO {
-    Connection conn = null;
-    PreparedStatement ps = null;
-    ResultSet rs = null;
-
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
