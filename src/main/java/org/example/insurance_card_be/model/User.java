910f55fde2cea949aa86de98c09ce259cd71577a
son12102003 <sondthe176576@fpt.edu.vn>
2024-05-24 15:08:26 +0700

    Son them file User

---
+++ src/main/java/org/example/insurance_card_be/model/User.java
@@ -0,0 +1,102 @@
+package org.example.insurance_card_be.model;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+    private String address;
+    private String role;
+    private String fullname;
+    private String gender;
+
+    public User() {
+    }
+
+    public User(int id, String username, String password, String email, String phone, String address, String role, String fullname, String gender){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.role = role;
+        this.fullname = fullname;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public  void setGender (String gender){
+        this.gender = gender;
+    }
+
+}
