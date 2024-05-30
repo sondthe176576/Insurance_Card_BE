92daaaa276f762d1921303c07500a81a70a9f6eb
son12102003 <sondthe176576@fpt.edu.vn>
2024-05-30 23:35:53 +0700

    Son them Login Google

--- src/main/java/org/example/insurance_card_be/controller/auth/LoginControl.java
+++ src/main/java/org/example/insurance_card_be/controller/auth/LoginControl.java
@@ -1,9 +1,15 @@
 package org.example.insurance_card_be.controller.auth;
 
-
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpSession;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
+import org.example.insurance_card_be.constant.Iconstant;
 import org.example.insurance_card_be.dao.implement.UserDAO;
+import org.example.insurance_card_be.entity.GoogleAccount;
 import org.example.insurance_card_be.model.Users;
 
 import jakarta.servlet.ServletException;
@@ -11,12 +17,63 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import java.util.logging.Logger;
 
 @WebServlet("/login")
 public class LoginControl extends HttpServlet {
     private UserDAO dao = new UserDAO();
 
+//    private static final Logger LOGGER = Logger.getLogger(LoginControl.class.getName());
+//
+//    public static String getToken(String code) throws ClientProtocolException, IOException {
+//
+//        String response = Request.Post(Iconstant.GOOGLE_LINK_GET_TOKEN)
+//
+//                .bodyForm(
+//
+//                        Form.form()
+//
+//                                .add("client_id", Iconstant.GOOGLE_CLIENT_ID)
+//
+//                                .add("client_secret", Iconstant.GOOGLE_CLIENT_SECRET)
+//
+//                                .add("redirect_uri", Iconstant.GOOGLE_REDIRECT_URI)
+//
+//                                .add("code", code)
+//
+//                                .add("grant_type", Iconstant.GOOGLE_GRANT_TYPE)
+//
+//                                .build()
+//
+//                )
+//
+//                .execute().returnContent().asString();
+//
+//
+//
+//        JsonObject jobj = new Gson().fromJson(response, JsonObject.class);
+//
+//        String accessToken = jobj.get("access_token").toString().replaceAll("\"", "");
+//
+//        return accessToken;
+//
+//    }
+//
+//    public static GoogleAccount getUserInfo(final String accessToken) throws ClientProtocolException, IOException {
+//        try {
+//            String link = Iconstant.GOOGLE_LINK_GET_USER_INFO + accessToken;
+//            String response = Request.Get(link).execute().returnContent().asString();
+//
+//            GoogleAccount googlePojo = new Gson().fromJson(response, GoogleAccount.class);
+//            return googlePojo;
+//        } catch (ClientProtocolException e) {
+//            LOGGER.severe("ClientProtocolException: " + e.getMessage());
+//            throw e;
+//        } catch (IOException e) {
+//            LOGGER.severe("IOException: " + e.getMessage());
+//            throw e;
+//        }
+//    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
